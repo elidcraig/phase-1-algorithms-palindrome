fa6a05c5e60a532a4fe9bd4b01f626260a487ec0
Eli Craig <elidcraig@gmail.com>
2022-05-07 14:49:43 -0700

    commit tree 21fd51dc92e738dedfd0cd6eb57b421f6bae87c3

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let wordArray = word.split('').reverse().join('')
+  return word === wordArray
 }
 
 /* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
+  isPalindrome takes in a string as an argument
+    make a copy of the string reversed
+      .split to split string into an array of individual characters
+      .reverse to reverse the order of the characters within the array
+      .join to put the array elements back together into a string
+    check if new string is strictly equal to original string, return the value  
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("madam"));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("palindrome"));
 }
 
 module.exports = isPalindrome;
